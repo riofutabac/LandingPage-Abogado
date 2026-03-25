65012313f7e31f0946251986561415952142a89a
riofutabac <alexis.lapo@epn.edu.ec>
2026-03-25 14:03:43 -0500

    feat: add sitemap and enhance .gitignore with new ignore patterns.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://tudominio.com",
+      url: "https://estudiojuridicoflapo.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
